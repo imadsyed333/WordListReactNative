3f36b6b5d2158478664aa187989aa45ad5cf6c53
Imad <creeperone23@gmail.com>
2023-05-04 10:36:56 -0400

    Style changes

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -3,23 +3,16 @@ import { Text, View, Button, StyleSheet } from "react-native";
 
 const Menu = ({ importWords, exportWords, clearWords }) => {
   return (
-    <View style={styles.container}>
-      <Text>Options </Text>
-      <Button
-        title="Import Words"
-        onPress={importWords}
-        style={{ backgroundColor: "#3C91E6" }}
-      />
-      <Button title="Export Words" onPress={exportWords} />
-      <Button title="Clear Words" onPress={clearWords} />
+    <View>
+      <Text style={styles.title}>Options </Text>
+      <Button title="Import Words" onPress={importWords} color={"#3C91E6"} />
+      <Button title="Export Words" onPress={exportWords} color={"#7CEA9C"} />
+      <Button title="Clear Words" onPress={clearWords} color={"#DE3C4B"} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "black",
-  },
   title: {
     color: "white",
     fontSize: 40,
